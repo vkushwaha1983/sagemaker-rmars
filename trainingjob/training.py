625a290a71d0acf3b144362c1e535661dfc16815
vkushwaha1983 <er.vivekkushwaha@gmail.com>
2019-02-13 12:49:29 +0000

    ok

---
+++ trainingjob/training.py
@@ -0,0 +1,81 @@
+import boto3
+import re
+import os
+import wget
+import time
+from time import gmtime, strftime
+import sys
+import json
+
+role = sys.argv[1]
+bucket = sys.argv[2]
+prefix = sys.argv[3]
+region =sys.argv[4]
+account = sys.argv[5]
+
+
+
+start = time.time()
+
+
+        
+s3 = boto3.client('s3')
+# create unique job name 
+r_job = 'sagemaker-bring-algor' + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
+
+print("Training job", r_job)
+
+r_training_params = {
+    "RoleArn": role,
+    "TrainingJobName": r_job,
+    "AlgorithmSpecification": {
+        "TrainingImage": '{}.dkr.ecr.{}.amazonaws.com/sagemaker-rmars:latest'.format(account, region),
+        "TrainingInputMode": "File"
+    },
+    "ResourceConfig": {
+        "InstanceCount": 1,
+        "InstanceType": "ml.m4.xlarge",
+        "VolumeSizeInGB": 10
+    },
+    "InputDataConfig": [
+        {
+            "ChannelName": "train",
+            "DataSource": {
+                "S3DataSource": {
+                    "S3DataType": "S3Prefix",
+                    "S3Uri": "s3://{}/{}/train".format(bucket, prefix),
+                    "S3DataDistributionType": "FullyReplicated"
+                }
+            },
+            "CompressionType": "None",
+            "RecordWrapperType": "None"
+        }
+    ],
+    "OutputDataConfig": {
+        "S3OutputPath": "s3://{}/{}/output".format(bucket, prefix)
+    },
+    "HyperParameters": {
+        "target": "Sepal.Length",
+        "degree": "2"
+    },
+    "StoppingCondition": {
+        "MaxRuntimeInSeconds": 60 * 60
+    }
+}
+# create the Amazon SageMaker training job
+sm = boto3.client('sagemaker')
+sm.create_training_job(**r_training_params)
+
+status = sm.describe_training_job(TrainingJobName=r_job)['TrainingJobStatus']
+print(status)
+sm.get_waiter('training_job_completed_or_stopped').wait(TrainingJobName=r_job)
+status = sm.describe_training_job(TrainingJobName=r_job)['TrainingJobStatus']
+print("Training job ended with status: " + status)
+if status == 'Failed':
+    message = sm.describe_training_job(TrainingJobName=r_job)['FailureReason']
+    print('Training failed with the following error: {}'.format(message))
+    raise Exception('Training job failed')
+
+
+end = time.time()
+print(end - start)
